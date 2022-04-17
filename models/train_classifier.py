16625a1e4ef545be6936347118a39c65192573e4
merocode8 <amira.elmansy@gmail.com>
2022-04-17 22:15:49 +0200

    Part 1 of train_classifier.py script

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,16 +1,67 @@
+from pyexpat import model
 import sys
+# download necessary NLTK data
+import nltk
+nltk.download(['punkt', 'wordnet', 'stopwords'])
+
+# import libraries
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from sqlalchemy import create_engine
+import pandas as pd
+import numpy as np
+import re
+import pickle
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report, accuracy_score
 
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table('messages', engine)
+    return df
 
 
 def tokenize(text):
-    pass
+    # Remove urls
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    text = re.sub(url_regex, 'urlplaceholder', text)
+    
+    # Convert to lower case and remove punctuation
+    text = re.sub(r'[^a-zA-Z0-9]', ' ' , text.lower()) 
+    
+    # Tokenize text
+    tokens = word_tokenize(text)
+    
+    # Remove stop words
+    tokens = [t for t in tokens if t not in stopwords.words('english')]
+    
+    # lemmatize tokens
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(t) for t in tokens]
+
+    return tokens
 
 
 def build_model():
-    pass
+    # Build pipeline
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)), 
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier(random_state=0, n_estimators=200)))
+    ])
+
+    # Use GridSearchCV to find best parameters 
+    parameters = { 'clf__estimator__criterion': ['gini', 'entropy'] }
+    model = GridSearchCV(pipeline, param_grid=parameters, n_jobs=2, verbose=3)
+
+    return model
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
